c3ea29fd3d086afe540d75352cb1c1024bf380e0
Enzo Guilherme <enzomarks77@gmail.com>
2025-06-13 04:27:28 -0300

    adicionei responsividade em tudo, criado o menu mobile, além de adicionar âncoras de suavização para o site

--- menu.js
+++ menu.js
@@ -1,3 +1,57 @@
+let btnMenu = document.getElementById("btn-menu")
+let menu = document.getElementById("menu-mobile")
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click',()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click',()=>{
+    menu.classList.remove('abrir-menu')
+})
+
+
+// Função para aplicar smooth scrolling
+function smoothScroll(target, duration) {
+    var targetSection = document.querySelector(target);
+    var targetPosition = targetSection.offsetTop;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
+
+// Adiciona evento de clique para as âncoras
+var links = document.querySelectorAll('nav a','.btn-contato');
+
+links.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        var target = this.getAttribute('href');
+        smoothScroll(target, 1000); // 1000 milliseconds = 1 segundo, ajuste conforme necessário
+    });
+});
+
 new Swiper('.card-wrapper', {
   loop: true,
   spaceBetween: 30,
