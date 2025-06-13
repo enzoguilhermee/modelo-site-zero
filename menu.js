9c54861009d11492b9893a275491a8ac7524df18
Enzo Guilherme <enzomarks77@gmail.com>
2025-06-13 03:09:42 -0300

    todas as sessões completas, agora é só deixar responsivo

--- menu.js
+++ menu.js
@@ -2,6 +2,51 @@ new Swiper('.card-wrapper', {
   loop: true,
   spaceBetween: 30,
 
+
+  autoplay: {
+    delay: 2500, // tempo entre os slides em ms (2.5 segundos)
+    disableOnInteraction: false, // continua mesmo se o usuário interagir
+  },
+
+  // Pagination Bullets 
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Breakpoints Responsivos
+  breakpoints: {
+    0:{
+        slidesPerView: 1
+    },
+
+    768:{
+        slidesPerView: 2
+    },
+
+    1024:{
+        slidesPerView: 3
+    },
+  }
+});
+
+new Swiper('.parceiros card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+
+  autoplay: {
+    delay: 2500, // tempo entre os slides em ms (2.5 segundos)
+    disableOnInteraction: false, // continua mesmo se o usuário interagir
+  },
+  
   // Pagination Bullets 
   pagination: {
     el: '.swiper-pagination',
