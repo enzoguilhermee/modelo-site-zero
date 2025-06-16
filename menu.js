9dbc15300058a3e0c548ca544747a7e33e21421e
Enzo Guilherme <enzomarks77@gmail.com>
2025-06-15 23:35:51 -0300

    corrigido grayscale do hover da sessão de parceiros

--- menu.js
+++ menu.js
@@ -88,44 +88,5 @@ new Swiper('.card-wrapper', {
     1024:{
         slidesPerView: 3
     },
-  }
-});
-
-new Swiper('.parceiros card-wrapper', {
-  loop: true,
-  spaceBetween: 30,
-
-
-  autoplay: {
-    delay: 2500, // tempo entre os slides em ms (2.5 segundos)
-    disableOnInteraction: false, // continua mesmo se o usuário interagir
   },
-  
-  // Pagination Bullets 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // Breakpoints Responsivos
-  breakpoints: {
-    0:{
-        slidesPerView: 1
-    },
-
-    768:{
-        slidesPerView: 2
-    },
-
-    1024:{
-        slidesPerView: 3
-    },
-  }
 });
